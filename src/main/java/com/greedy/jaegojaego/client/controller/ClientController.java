e417bb6a36d6515da408e4b86f0efcadda7379e5
DESKTOP-PMPHT2S\kidol <kidoli199311@gmail.com>
2022-04-17 22:25:17 +0900

    type: feat 거래처 관리 comment : 거래처관리 view페이지 초기 작업 중

---
+++ src/main/java/com/greedy/jaegojaego/client/controller/ClientController.java
@@ -0,0 +1,25 @@
+package com.greedy.jaegojaego.client.controller;
+
+import com.greedy.jaegojaego.client.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/client")
+public class ClientController {
+
+    private final ClientService clientService;
+
+    @Autowired
+    public ClientController(ClientService clientService){this.clientService = clientService; }
+
+    @GetMapping("/list")
+    public ModelAndView clientList(){
+
+        return new ModelAndView("/client/clientList")
+    }
+
+}
