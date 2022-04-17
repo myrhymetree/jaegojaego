c08bffa69958a373bad59c5aba185f6cb71002ec
Park-In-Geun <43868420+Park-In-Geun@users.noreply.github.com>
2022-04-17 22:22:49 +0900

    type: feat
    comment :  공통 스크립트 파일 footer 경로 수정, order 관련 controller, service, 본사 발주 목록/신청 , 가맹점 발주 목록 html 페이지 생성

---
+++ src/main/java/com/greedy/jaegojaego/order/model/controller/OrderController.java
@@ -0,0 +1,47 @@
+package com.greedy.jaegojaego.order.model.controller;
+
+import com.greedy.jaegojaego.order.model.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/order")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/companyorderlist")
+    public ModelAndView selectCompanyOrderList(ModelAndView mv) {
+
+        mv.setViewName("/order/companyOrderList");
+
+        return mv;
+    }
+
+    @GetMapping("/franchiseorderlist")
+    public ModelAndView selectFranchiseOrderList(ModelAndView mv) {
+
+        mv.setViewName("/order/franchiseOrderList");
+
+        return mv;
+    }
+
+    @GetMapping("/companyorderregist")
+    public ModelAndView companyOrderRegist(ModelAndView mv) {
+
+        mv.setViewName("/order/companyOrderRegist");
+
+        return mv;
+    }
+
+
+}
