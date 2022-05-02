34a7edef6eccc52ff182476e82cf56cffaa43f6c
Park-In-Geun <43868420+Park-In-Geun@users.noreply.github.com>
2022-05-03 00:03:14 +0900

    Revert "Revert "type: feat, fix""
    This reverts commit 0130d72e261b5ee84f51d501319f1ac1218dfa03.

--- src/main/java/com/greedy/jaegojaego/order/order/model/dto/company/OrderApplicationDTO.java
+++ src/main/java/com/greedy/jaegojaego/order/order/model/dto/company/OrderApplicationDTO.java
@@ -11,15 +11,17 @@ public class OrderApplicationDTO {
     private int orderApplicationNo;
     private OrderClientDTO client;
     private List<OrderApplicationItemDTO> orderApplicationItemList;
+    private String orderApplicationAddress;
 
     public OrderApplicationDTO() {
     }
 
-    public OrderApplicationDTO(CompanyOrderHistoryDTO companyOrderHistory, int orderApplicationNo, OrderClientDTO client, List<OrderApplicationItemDTO> orderApplicationItemList) {
+    public OrderApplicationDTO(CompanyOrderHistoryDTO companyOrderHistory, int orderApplicationNo, OrderClientDTO client, List<OrderApplicationItemDTO> orderApplicationItemList, String orderApplicationAddress) {
         this.companyOrderHistory = companyOrderHistory;
         this.orderApplicationNo = orderApplicationNo;
         this.client = client;
         this.orderApplicationItemList = orderApplicationItemList;
+        this.orderApplicationAddress = orderApplicationAddress;
     }
 
     public CompanyOrderHistoryDTO getCompanyOrderHistory() {
@@ -54,13 +56,21 @@ public class OrderApplicationDTO {
         this.orderApplicationItemList = orderApplicationItemList;
     }
 
+    public String getOrderApplicationAddress() {
+        return orderApplicationAddress;
+    }
+
+    public void setOrderApplicationAddress(String orderApplicationAddress) {
+        this.orderApplicationAddress = orderApplicationAddress;
+    }
+
     @Override
     public String toString() {
         return "OrderApplicationDTO{" +
-                "CompanyOrderHistoryNo=" + companyOrderHistory.getCompanyOrderHistoryNo() +
-                ", orderApplicationNo=" + orderApplicationNo +
+                "orderApplicationNo=" + orderApplicationNo +
                 ", client=" + client +
                 ", orderApplicationItemList=" + orderApplicationItemList +
+                ", orderApplicationAddress='" + orderApplicationAddress + '\'' +
                 '}';
     }
 }
--- src/test/java/com/greedy/jaegojaego/order/order/model/service/OrderServiceTests.java
+++ src/test/java/com/greedy/jaegojaego/order/order/model/service/OrderServiceTests.java
@@ -3,22 +3,23 @@ package com.greedy.jaegojaego.order.order.model.service;
 import com.greedy.jaegojaego.config.BeanConfiguration;
 import com.greedy.jaegojaego.config.JaegojaegoApplication;
 import com.greedy.jaegojaego.config.JpaConfiguration;
-import com.greedy.jaegojaego.franchise.entity.FranchiseInfo;
 import com.greedy.jaegojaego.order.client.model.entity.OrderClient;
 import com.greedy.jaegojaego.order.client.model.entity.OrderClientContractItem;
 import com.greedy.jaegojaego.order.client.model.repository.OrderClientContractItemRepository;
 import com.greedy.jaegojaego.order.company.model.entity.OrderCompanyAccount;
 import com.greedy.jaegojaego.order.franchise.model.entity.OrderFranchiseInfo;
-import com.greedy.jaegojaego.order.franchise.model.repository.FranchiseInfoRepository;
+import com.greedy.jaegojaego.order.franchise.model.repository.OrderFranchiseInfoRepository;
 import com.greedy.jaegojaego.order.item.model.entity.OrderItemInfo;
 import com.greedy.jaegojaego.order.item.model.repository.OrderItemInfoRepository;
 import com.greedy.jaegojaego.order.order.model.entity.company.*;
 import com.greedy.jaegojaego.order.order.model.entity.franchise.FranchiseOrder;
+import com.greedy.jaegojaego.order.order.model.entity.franchise.FranchiseOrderStatusHistory;
 import com.greedy.jaegojaego.order.order.model.repository.company.CompanyOrderHistoryRepository;
 import com.greedy.jaegojaego.order.order.model.repository.company.CompanyOrderItemRepository;
 import com.greedy.jaegojaego.order.order.model.repository.company.OrderApplicationItemRepository;
 import com.greedy.jaegojaego.order.order.model.repository.company.OrderApplicationRepository;
 import com.greedy.jaegojaego.order.order.model.repository.franchise.FranchiseOrderRepository;
+import com.greedy.jaegojaego.order.order.model.repository.franchise.FranchiseOrderStatusHistoryRepository;
 import com.greedy.jaegojaego.order.warehouse.repository.OrderItemWarehouseRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,12 +28,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @ContextConfiguration(classes = {JpaConfiguration.class, JaegojaegoApplication.class, BeanConfiguration.class, JaegojaegoApplication.class})
@@ -64,7 +67,10 @@ public class OrderServiceTests {
     private FranchiseOrderRepository franchiseOrderRepository;
 
     @Autowired
-    private FranchiseInfoRepository franchiseInfoRepository;
+    private OrderFranchiseInfoRepository orderFranchiseInfoRepository;
+
+    @Autowired
+    private FranchiseOrderStatusHistoryRepository franchiseOrderStatusHistoryRepository;
 
     @Test
     @DisplayName("본사 발주 내역 목록 조회 메소드 테스트")
@@ -146,6 +152,7 @@ public class OrderServiceTests {
 
     @Test
     @DisplayName("본사 발주 신청 메소드 테스트")
+    @Transactional
     public void insertCompanyOrder() {
 
         //given
@@ -154,9 +161,8 @@ public class OrderServiceTests {
         int itemInfoNo = 1;
         int memberNo = 2;
         int clientNo = 1;
-        int companyOrderHistoryNo = 70;
-        int orderApplicationNo = 50;
 
+        //when
         //발주 내역 관련
         OrderCompanyAccount companyAccount = new OrderCompanyAccount();
         companyAccount.setMemberNo(memberNo);
@@ -164,6 +170,11 @@ public class OrderServiceTests {
         CompanyOrderHistory companyOrderHistory = new CompanyOrderHistory();
         companyOrderHistory.setCompanyOrderHistoryCreatedDate(new Date(System.currentTimeMillis()));
         companyOrderHistory.setOrderCompanyAccount(companyAccount);
+        companyOrderHistory.setCompanyOrderHistoryStaus("BEFORE");
+
+        companyOrderHistoryRepository.save(companyOrderHistory); //발주 내역 생성
+
+        int companyOrderHistoryNo = companyOrderHistoryRepository.selectRecentHistoryNo();
 
         //발주 내역 물품 관련
         CompanyOrderHistory companyOrderItemTest = new CompanyOrderHistory();
@@ -173,16 +184,18 @@ public class OrderServiceTests {
         newOrderItemInfo.setItemInfoNo(itemInfoNo);
 
         CompanyOrderItemPK companyOrderItemPK = new CompanyOrderItemPK();
-        companyOrderItemPK.setCompanyOrderHistory(companyOrderItemTest);
+        companyOrderItemPK.setCompanyOrderHistory(companyOrderHistory);
         companyOrderItemPK.setOrderItemInfo(newOrderItemInfo);
 
         CompanyOrderItem companyOrderItem = new CompanyOrderItem();
         companyOrderItem.setCompanyOrderItemPK(companyOrderItemPK);
         companyOrderItem.setCompanyOrderItemAmount(itemAmount);
 
+        companyOrderItemRepository.save(companyOrderItem);       //발주 내역 물품 생성
+
         //발주 신청서 관련
         CompanyOrderHistory orderApplicationTest = new CompanyOrderHistory();
-        companyOrderHistory.setCompanyOrderHistoryNo(companyOrderHistoryNo);
+        orderApplicationTest.setCompanyOrderHistoryNo(companyOrderHistoryNo);
 
         OrderClient orderClient = new OrderClient();
         orderClient.setClientNo(clientNo);
@@ -191,7 +204,11 @@ public class OrderServiceTests {
         orderApplication.setOrderClient(orderClient);
         orderApplication.setCompanyOrderHistory(orderApplicationTest);
 
+        orderApplicationRepository.save(orderApplication);       //발주 신청서 생성
+
         //발주 신청서 별 물품 관련
+
+        int orderApplicationNo = orderApplicationRepository.selectRecentOrderApplication(2).get(0);
 
         OrderApplication orderApplicationItemTest = new OrderApplication();
         OrderClientContractItem orderClientContractItem = new OrderClientContractItem();
@@ -207,25 +224,24 @@ public class OrderServiceTests {
         orderApplicationItem.setOrderApplicationItemPK(orderApplicationItemPK);
         orderApplicationItem.setOrderApplicationItemAmount(itemAmount);
 
-        //when
-        companyOrderHistoryRepository.save(companyOrderHistory); //발주 내역 생성
-        companyOrderItemRepository.save(companyOrderItem);       //발주 내역 물품 생성
-        orderApplicationRepository.save(orderApplication);       //발주 신청서 생성
         orderApplicationItemRepository.save(orderApplicationItem);
 
         //then
-
-
+        assertNotNull(companyOrderHistoryRepository.findById(companyOrderHistoryNo).get());
+        assertNotNull(companyOrderItemRepository.findByCompanyOrderHistoryNo(companyOrderHistoryNo));
+        assertNotNull(orderApplicationRepository.findByCompanyOrderHistory_CompanyOrderHistoryNo(companyOrderHistoryNo));
+        assertNotNull(orderApplicationItemRepository.findByOrderApplicationNo(orderApplicationNo));
     }
 
     @Test
     @DisplayName("본사 발주 처리 상태 변경 메소드 테스트")
+    @Transactional
     public void updateCompanyOrderHistoryStatus() {
 
         //given
         int memberNo = 107;
         int companyOrderHistoryNo = 1;
-        String orderStatus = "COMPLETE";
+        String orderStatus = "REJECT";
 
         //when
         OrderCompanyAccount orderCompanyAccount = new OrderCompanyAccount();
@@ -237,6 +253,9 @@ public class OrderServiceTests {
         companyOrderHistory.setCompanyOrderHistoryStatusDate(new Date(System.currentTimeMillis()));
 
         //then
+        CompanyOrderHistory checkCompanyOrderHistory = companyOrderHistoryRepository.findById(companyOrderHistoryNo).get();
+
+        assertTrue(orderStatus.equals(checkCompanyOrderHistory.getCompanyOrderHistoryStaus()));
 
     }
 
@@ -255,7 +274,7 @@ public class OrderServiceTests {
         //when
         if("본사".equals(memberDivision)) {
 
-            franchiseNoList = franchiseInfoRepository.findByHeadOfficeSupervisor_MemberNo(memberNo);
+            franchiseNoList = orderFranchiseInfoRepository.findByHeadOfficeSupervisor_MemberNo(memberNo);
 
             for(int i = 0; i < franchiseNoList.size(); i++) {
 
@@ -292,6 +311,87 @@ public class OrderServiceTests {
         //then
         assertNotNull(franchiseOrder);
         assertNotNull(franchiseOrder.getFranchiseOrderItemList());
+
+    }
+
+    @Test
+    @DisplayName("본사 발주 내역 처리 상태 승인완료 변경 메소드 테스트")
+    @Transactional
+    public void updateFranchiseOrderStatusComplete() {
+
+        //given
+        int memberNo = 107;
+        int franchiseOrderNo = 21;
+        String orderStatus = "COMPLETE";
+
+        //when
+        FranchiseOrder franchiseOrder = franchiseOrderRepository.findById(franchiseOrderNo).get();
+        franchiseOrder.setFranchiseOrderStatus(orderStatus);
+        franchiseOrder.setFranchiseOrderStatusDate(new Date(System.currentTimeMillis()));
+
+        OrderCompanyAccount orderCompanyAccount = new OrderCompanyAccount();
+        orderCompanyAccount.setMemberNo(memberNo);
+
+        FranchiseOrderStatusHistory franchiseOrderStatusHistory = new FranchiseOrderStatusHistory();
+        franchiseOrderStatusHistory.setFranchiseOrderNo(franchiseOrderNo);
+        franchiseOrderStatusHistory.setOrderCompanyAccount(orderCompanyAccount);
+        franchiseOrderStatusHistory.setFranchiseOrderStatus(orderStatus);
+        franchiseOrderStatusHistory.setFranchiseOrderStatusHistoryDate(new Date(System.currentTimeMillis()));
+
+        franchiseOrderStatusHistoryRepository.save(franchiseOrderStatusHistory);
+
+        //then
+        assertTrue("COMPLETE".equals(franchiseOrderRepository.findById(franchiseOrderNo).get().getFranchiseOrderStatus()));
+        assertNotNull(franchiseOrderStatusHistoryRepository.findByFranchiseOrderNo(franchiseOrderNo));
+
+    }
+
+    @Test
+    @DisplayName("본사 발주 내역 처리 상태 승인거절 변경 메소드 테스트")
+    @Transactional
+    public void updateFranchiseOrderStatusReject() {
+
+        //given
+        int memberNo = 107;
+        int franchiseOrderNo = 21;
+        String orderStatus = "REJECT";
+        String rejectMessage = "거부 테스트";
+
+        //when
+        FranchiseOrder franchiseOrder = franchiseOrderRepository.findById(franchiseOrderNo).get();
+        franchiseOrder.setFranchiseOrderStatus(orderStatus);
+        franchiseOrder.setFranchiseOrderStatusDate(new Date(System.currentTimeMillis()));
+        franchiseOrder.setFranchiseOrderStatusRejectionContent(rejectMessage);
+
+        OrderCompanyAccount orderCompanyAccount = new OrderCompanyAccount();
+        orderCompanyAccount.setMemberNo(memberNo);
+
+        FranchiseOrderStatusHistory franchiseOrderStatusHistory = new FranchiseOrderStatusHistory();
+        franchiseOrderStatusHistory.setFranchiseOrderNo(franchiseOrderNo);
+        franchiseOrderStatusHistory.setOrderCompanyAccount(orderCompanyAccount);
+        franchiseOrderStatusHistory.setFranchiseOrderStatus(orderStatus);
+        franchiseOrderStatusHistory.setFranchiseOrderStatusHistoryDate(new Date(System.currentTimeMillis()));
+
+        franchiseOrderStatusHistoryRepository.save(franchiseOrderStatusHistory);
+
+        //then
+        assertTrue("REJECT".equals(franchiseOrderRepository.findById(franchiseOrderNo).get().getFranchiseOrderStatus()));
+        assertNotNull(franchiseOrderStatusHistoryRepository.findByFranchiseOrderNo(franchiseOrderNo));
+
+    }
+
+    @Test
+    @DisplayName("가맹점 발주 거절 사유서 조회 메소드 테스트")
+    public void selectRejectContent() {
+
+        //given
+        int franchiseOrderNo = 21;
+
+        //when
+        FranchiseOrder franchiseOrder = franchiseOrderRepository.findById(franchiseOrderNo).get();
+
+        //then
+        assertNotNull(franchiseOrder.getFranchiseOrderStatusRejectionContent());
 
     }
 
