1e7e0dcfbca5e98fb1137096e016f5e2893e31a7
DESKTOP-MOB8F55\tj <myrhymetree@gmail.com>
2022-05-13 21:58:45 +0900

    type : fix comment : 헤더 수정

--- src/main/resources/static/js/common/memberInfo.js
+++ src/main/resources/static/js/common/memberInfo.js
@@ -1,58 +1,61 @@
 $(document).ready(function(){
 
-    $(function () {
-        $.ajax({
-            url: "/member/loginMemberInformation",
-            success: function (data) {
+    // $(".dropdown-menu").click( function () {
 
-                if(data.memberDivision == "본사") {
+        $(function () {
+            $.ajax({
+                url: "/member/loginMemberInformation",
+                success: function (data) {
 
-                    console.log(data);
+                    if(data.memberDivision == "본사") {
 
-                    $("#memberInfo-memberName").val(data.memberName);
-                    $("#memberInfo-cellPhone").val(data.memberCellPhone);
-                    $("#memberInfo-officePhone").val(data.officePhoneNumber);
-                    $("#memberInfo-email").val(data.memberEmail);
-                    $("#memberInfo-department").val(data.department.departmentName);
-                    $("#memberInfo-cellPhone").val(data.memberCellPhone);
-                    $("#memberInfo-officePhone").val(data.officePhoneNumber);
+                        console.log(data);
 
-                } else if(data.memberDivision == "가맹점" && data.officeDivision == "대표자") {
+                        $("#memberInfo-memberName").val(data.memberName);
+                        $("#memberInfo-cellPhone").val(data.memberCellPhone);
+                        $("#memberInfo-officePhone").val(data.officePhoneNumber);
+                        $("#memberInfo-email").val(data.memberEmail);
+                        $("#memberInfo-department").val(data.department.departmentName);
+                        $("#memberInfo-cellPhone").val(data.memberCellPhone);
+                        $("#memberInfo-officePhone").val(data.officePhoneNumber);
 
-                    console.log(data);
-                    console.log(data.supervisor)
-                    $("#franchiseInfo-memberName").val(data.representativeName);
-                    $("#franchiseInfo-cellPhone").val(data.representativePhone);
-                    $("#franchiseInfo-franchisePhone").val(data.phone);
-                    $("#franchiseInfo-email").val(data.representativeEmail);
-                    $("#franchiseInfo-branchName").val(data.branchName);
-                    $("#franchiseInfo-businessRegistrationNo").val(data.businessRegistrationNo);
-                    $("#franchiseInfo-address").val(data.address);
-                    $("#franchiseInfo-bankAccountNo").val(data.bankAccountNo);
-                    $("#franchiseInfo-supervisor").val(data.supervisor.memberName);
-                    $("#franchiseInfo-department").val(data.supervisor.department.departmentName);
+                    } else if(data.memberDivision == "가맹점" && data.officeDivision == "대표자") {
 
-                } else if(data.memberDivision == "가맹점" && data.officeDivision == "직원") {
+                        console.log(data);
+                        console.log(data.supervisor)
+                        $("#franchiseInfo-memberName").val(data.representativeName);
+                        $("#franchiseInfo-cellPhone").val(data.representativePhone);
+                        $("#franchiseInfo-franchisePhone").val(data.phone);
+                        $("#franchiseInfo-email").val(data.representativeEmail);
+                        $("#franchiseInfo-branchName").val(data.branchName);
+                        $("#franchiseInfo-businessRegistrationNo").val(data.businessRegistrationNo);
+                        $("#franchiseInfo-address").val(data.address);
+                        $("#franchiseInfo-bankAccountNo").val(data.bankAccountNo);
+                        $("#franchiseInfo-supervisor").val(data.supervisor.memberName);
+                        $("#franchiseInfo-department").val(data.supervisor.department.departmentName);
 
-                    console.log(data);
-                    console.log(data.managerPhone)
-                    $("#managerInfo-memberName").val(data.managerName);
-                    $("#managerInfo-cellPhone").val(data.managerPhone);
-                    $("#managerInfo-branchName").val(data.franchiseInfo.branchName);
-                    $("#managerInfo-supervisor").val(data.franchiseInfo.supervisor.memberName);
-                    $("#managerInfo-department").val(data.franchiseInfo.supervisor.department.departmentName);
-                    $("#managerInfo-address").val(data.franchiseInfo.address);
-                    $("#managerInfo-franchisePhone").val(data.franchiseInfo.phone);
-                    $("#managerInfo-email1").val(data.managerEmail);
+                    } else if(data.memberDivision == "가맹점" && data.officeDivision == "직원") {
 
+                        console.log(data);
+                        console.log(data.managerPhone)
+                        $("#managerInfo-memberName").val(data.managerName);
+                        $("#managerInfo-cellPhone").val(data.managerPhone);
+                        $("#managerInfo-branchName").val(data.franchiseInfo.branchName);
+                        $("#managerInfo-supervisor").val(data.franchiseInfo.supervisor.memberName);
+                        $("#managerInfo-department").val(data.franchiseInfo.supervisor.department.departmentName);
+                        $("#managerInfo-address").val(data.franchiseInfo.address);
+                        $("#managerInfo-franchisePhone").val(data.franchiseInfo.phone);
+                        $("#managerInfo-email1").val(data.managerEmail);
+
+                    }
+
+                },
+                error:function (xhr) {
+                    console.log(xhr);
                 }
-
-            },
-            error:function (xhr) {
-                console.log(xhr);
-            }
+            });
         });
-    });
 
+    // });
 });
 
